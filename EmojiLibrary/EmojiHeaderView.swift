11fc1f3d66d457cd9e9f0f4cbca2bd0fc495025b
maria <maria@maria.dev>
2020-12-10 17:11:34 +0100

    Add base

---
+++ EmojiLibrary/EmojiHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiHeaderView.swift
+//  EmojiLibrary
+//
+//  Created by Maria on 03/12/2020.
+//
+
+import UIKit
+
+class EmojiHeaderView: UICollectionReusableView {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    static let reuseIdentifier = String(describing: EmojiHeaderView.self)
+}
